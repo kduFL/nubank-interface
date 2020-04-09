ab150d89b41aeee917243cad017a9344880a180f
kdu <kdu.eduardo97@gmail.com>
2020-04-09 18:56:56 -0300

    Add estilos e animações

---
+++ src/components/Tabs/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {Container, TabsContainer, TabItem, TabText} from './styles';
+
+export default function Tabs({translateY}) {
+  return (
+    <Container style={{
+      transform: [{
+        translateY: translateY.interpolate({
+          inputRange: [0, 380],
+          outputRange: [0, 30],
+          extrapolate: 'clamp'
+        })
+      }],
+      opacity: translateY.interpolate({
+        inputRange: [0, 60],
+        outputRange: [1, 0.1],
+        extrapolate: 'clamp'
+      })
+    }}>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#fff" />
+          <TabText>Indicar Amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="chat-bubble-outline" size={24} color="#fff" />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-upward" size={24} color="#fff" />
+          <TabText>Transferir</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="lock" size={24} color="#fff" />
+          <TabText>Bloquear cartão</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import React from 'react';
 import '~/config/ReactotronConfig';
 
 import Routes from '~/routes';
+import { StatusBar } from 'react-native';
 
-const App = () => <Routes />;
+const App = () => (
+  <>
+    <StatusBar barStyle='light-content' backgroundColor='#8b10ae' />
+    <Routes />
+  </>
+);
 
 export default App;
